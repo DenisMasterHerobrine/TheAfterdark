62d6ab5ee26e67d0f76f9c1940c5d4dcefa1ef56
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2024-09-13 20:19:52 +0500

    chore: rename all classes, begin working

--- fabric/src/main/java/dev/denismasterherobrine/fabric/client/TheAfterdarkFabricClient.java
+++ fabric/src/main/java/dev/denismasterherobrine/fabric/client/TheAfterdarkFabricClient.java
@@ -2,7 +2,7 @@ package dev.denismasterherobrine.fabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public final class ExampleModFabricClient implements ClientModInitializer {
+public final class TheAfterdarkFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
