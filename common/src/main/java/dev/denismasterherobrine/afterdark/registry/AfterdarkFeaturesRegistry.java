42b14983221cb2063ff5642ecabbe080234e6d77
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2024-09-29 21:37:50 +0500

    feat: implemented ore-rich stalactites and stalagmites

--- common/src/main/java/dev/denismasterherobrine/afterdark/registry/AfterdarkFeaturesRegistry.java
+++ common/src/main/java/dev/denismasterherobrine/afterdark/registry/AfterdarkFeaturesRegistry.java
@@ -1,6 +1,5 @@
 package dev.denismasterherobrine.afterdark.registry;
 
-import dev.architectury.injectables.annotations.PlatformOnly;
 import dev.denismasterherobrine.afterdark.TheAfterdark;
 import dev.denismasterherobrine.afterdark.features.*;
 import dev.denismasterherobrine.afterdark.features.configuration.*;
@@ -37,6 +36,17 @@ public class AfterdarkFeaturesRegistry {
     public static final Feature<LargeDripstoneFeatureConfig> LARGE_ICE_PILLAR = new LargeIcePillarFeature(LargeDripstoneFeatureConfig.CODEC);
     public static final Feature<LargeDripstoneFeatureConfig> LARGE_BLUE_ICE_PILLAR = new LargeBlueIcePillarFeature(LargeDripstoneFeatureConfig.CODEC);
 
+    // Raw Ore Pillars
+    public static final Feature<LargeDripstoneFeatureConfig> LARGE_RAW_IRON_PILLAR = new LargeRawIronPillarFeature(LargeDripstoneFeatureConfig.CODEC);
+    public static final Feature<LargeDripstoneFeatureConfig> LARGE_RAW_GOLD_PILLAR = new LargeRawGoldPillarFeature(LargeDripstoneFeatureConfig.CODEC);
+    public static final Feature<LargeDripstoneFeatureConfig> LARGE_RAW_COPPER_PILLAR = new LargeRawCopperPillarFeature(LargeDripstoneFeatureConfig.CODEC);
+
+    // Ore Pillars
+    public static final Feature<LargeDripstoneFeatureConfig> LARGE_DIAMOND_ORE_PILLAR = new LargeDiamondOrePillarFeature(LargeDripstoneFeatureConfig.CODEC);
+    public static final Feature<LargeDripstoneFeatureConfig> LARGE_EMERALD_ORE_PILLAR = new LargeEmeraldOrePillarFeature(LargeDripstoneFeatureConfig.CODEC);
+    public static final Feature<LargeDripstoneFeatureConfig> LARGE_LAPIS_ORE_PILLAR = new LargeLapisOrePillarFeature(LargeDripstoneFeatureConfig.CODEC);
+    public static final Feature<LargeDripstoneFeatureConfig> LARGE_REDSTONE_ORE_PILLAR = new LargeRedstoneOrePillarFeature(LargeDripstoneFeatureConfig.CODEC);
+
     public static void register() {
         Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "large_basalt_pillar"), LARGE_BASALT_PILLAR);
         Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "additive_ground_blob"), ADDITIVE_GROUND_BLOB);
@@ -60,5 +70,16 @@ public class AfterdarkFeaturesRegistry {
         Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "large_packed_ice_pillar"), LARGE_PACKED_ICE_PILLAR);
         Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "large_ice_pillar"), LARGE_ICE_PILLAR);
         Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "large_blue_ice_pillar"), LARGE_BLUE_ICE_PILLAR);
+
+        // Raw Ore Pillars
+        Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "raw_iron_pillar"), LARGE_RAW_IRON_PILLAR);
+        Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "raw_gold_pillar"), LARGE_RAW_GOLD_PILLAR);
+        Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "raw_copper_pillar"), LARGE_RAW_COPPER_PILLAR);
+
+        // Ore Pillars
+        Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "diamond_ore_pillar"), LARGE_DIAMOND_ORE_PILLAR);
+        Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "emerald_ore_pillar"), LARGE_EMERALD_ORE_PILLAR);
+        Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "lapis_ore_pillar"), LARGE_LAPIS_ORE_PILLAR);
+        Registry.register(Registries.FEATURE, new Identifier(TheAfterdark.MOD_ID, "redstone_ore_pillar"), LARGE_REDSTONE_ORE_PILLAR);
     }
 }
