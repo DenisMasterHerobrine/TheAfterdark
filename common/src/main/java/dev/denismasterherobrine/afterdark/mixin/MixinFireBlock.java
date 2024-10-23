924a920d071236961a9680971c16816136179029
VeiTrr <72299236+VeiTrr@users.noreply.github.com>
2024-10-23 03:16:41 +0300

    fix: forge mixin issue 💀

--- common/src/main/java/dev/denismasterherobrine/afterdark/mixin/MixinFireBlock.java
+++ common/src/main/java/dev/denismasterherobrine/afterdark/mixin/MixinFireBlock.java
@@ -17,15 +17,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FireBlock.class)
 public class MixinFireBlock {
-    @Inject(method = "trySpreadingFire", at = @At("HEAD"), cancellable = true)
-    private void preventGrassBurning(World world, BlockPos pos, int spreadFactor, Random random, int currentAge, CallbackInfo ci) {
-        if (world.getServer() != null) {
-            if (world == world.getServer().getWorld(AfterdarkRegistry.AFTERDARK_LEVEL)
-                    && Config.INSTANCE.GrassBlocks.contains(Registries.BLOCK.getId(world.getBlockState(pos).getBlock()).toString()) && !Config.INSTANCE.shouldGrassBurn) {
-                ci.cancel();
-            }
-        }
-    }
+//    @Inject(method = "trySpreadingFire", at = @At("HEAD"), cancellable = true)
+//    private void preventGrassBurning(World world, BlockPos pos, int spreadFactor, Random random, int currentAge, CallbackInfo ci) {
+//        if (world.getServer() != null) {
+//            if (world == world.getServer().getWorld(AfterdarkRegistry.AFTERDARK_LEVEL)
+//                    && Config.INSTANCE.GrassBlocks.contains(Registries.BLOCK.getId(world.getBlockState(pos).getBlock()).toString()) && !Config.INSTANCE.shouldGrassBurn) {
+//                ci.cancel();
+//            }
+//        }
+//    }
 
     @Inject(method = "areBlocksAroundFlammable", at = @At("HEAD"), cancellable = true)
     private void preventGrassBurning(BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
