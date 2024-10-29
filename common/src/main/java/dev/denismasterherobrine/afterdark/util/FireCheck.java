8fc03720d92ad1e3fa943d88eef57a2fe838e0cf
VeiTrr <72299236+VeiTrr@users.noreply.github.com>
2024-10-29 18:24:58 +0300

    fix: client check on serverside is not good

--- common/src/main/java/dev/denismasterherobrine/afterdark/util/FireCheck.java
+++ common/src/main/java/dev/denismasterherobrine/afterdark/util/FireCheck.java
@@ -2,17 +2,18 @@ package dev.denismasterherobrine.afterdark.util;
 
 import dev.denismasterherobrine.afterdark.Config;
 import dev.denismasterherobrine.afterdark.registry.AfterdarkRegistry;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.registry.Registries;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 public class FireCheck {
-    public static void GrassAroundCheck(BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        if (MinecraftClient.getInstance().getServer() != null) {
-            if (world == MinecraftClient.getInstance().getServer().getWorld(AfterdarkRegistry.AFTERDARK_LEVEL) && !Config.INSTANCE.shouldGrassBurn) {
+    public static void GrassAroundCheck(ServerWorld world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (!world.isClient) {
+            MinecraftServer server = world.getServer();
+            if (world == server.getWorld(AfterdarkRegistry.AFTERDARK_LEVEL) && !Config.INSTANCE.shouldGrassBurn) {
                 for (Direction direction : Direction.values()) {
                     if (Config.INSTANCE.GrassBlocks.contains(Registries.BLOCK.getId(world.getBlockState(pos.offset(direction)).getBlock()).toString())) {
                         cir.setReturnValue(false);
