f7e4352c367b31ddbc0d946e7404e9831fa77705
VeiTrr <72299236+VeiTrr@users.noreply.github.com>
2024-10-31 06:21:53 +0300

    fix: cast to server class on client is not good

--- common/src/main/java/dev/denismasterherobrine/afterdark/mixin/MixinFireBlock.java
+++ common/src/main/java/dev/denismasterherobrine/afterdark/mixin/MixinFireBlock.java
@@ -2,9 +2,9 @@ package dev.denismasterherobrine.afterdark.mixin;
 
 import dev.denismasterherobrine.afterdark.util.FireCheck;
 import net.minecraft.block.FireBlock;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +15,7 @@ public class MixinFireBlock {
 
     @Inject(method = "areBlocksAroundFlammable", at = @At("HEAD"), cancellable = true)
     private void preventGrassBurning(BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        FireCheck.GrassAroundCheck((ServerWorld) world, pos, cir);
+        FireCheck.GrassAroundCheck((World) world, pos, cir);
     }
 
 }
