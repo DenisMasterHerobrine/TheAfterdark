7ebe806b4a3c5e18adc38e5adbb8cad074d6284e
VeiTrr <72299236+VeiTrr@users.noreply.github.com>
2024-10-31 06:21:53 +0300

    fix: cast to server class on client is not good

--- common/src/main/java/dev/denismasterherobrine/afterdark/util/FireCheck.java
+++ common/src/main/java/dev/denismasterherobrine/afterdark/util/FireCheck.java
@@ -4,19 +4,24 @@ import dev.denismasterherobrine.afterdark.Config;
 import dev.denismasterherobrine.afterdark.registry.AfterdarkRegistry;
 import net.minecraft.registry.Registries;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 public class FireCheck {
-    public static void GrassAroundCheck(ServerWorld world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+    public static void GrassAroundCheck(World world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         if (!world.isClient) {
             MinecraftServer server = world.getServer();
-            if (world == server.getWorld(AfterdarkRegistry.AFTERDARK_LEVEL) && !Config.INSTANCE.shouldGrassBurn) {
-                for (Direction direction : Direction.values()) {
-                    if (Config.INSTANCE.GrassBlocks.contains(Registries.BLOCK.getId(world.getBlockState(pos.offset(direction)).getBlock()).toString())) {
-                        cir.setReturnValue(false);
+            if (server != null) {
+                if (world == server.getWorld(AfterdarkRegistry.AFTERDARK_LEVEL) && !Config.INSTANCE.shouldGrassBurn) {
+                    for (Direction direction : Direction.values()) {
+                        if (Config.INSTANCE.GrassBlocks.contains(Registries.BLOCK.getId(world.getBlockState(pos.offset(direction)).getBlock()).toString())
+                                || Config.INSTANCE.GrassBlocks.contains(Registries.BLOCK.getId(world.getBlockState(pos.up()).getBlock()).toString())
+                                || Config.INSTANCE.GrassBlocks.contains(Registries.BLOCK.getId(world.getBlockState(pos.down()).getBlock()).toString())
+                                || Config.INSTANCE.GrassBlocks.contains(Registries.BLOCK.getId(world.getBlockState(pos).getBlock()).toString())) {
+                            cir.setReturnValue(false);
+                        }
                     }
                 }
             }
